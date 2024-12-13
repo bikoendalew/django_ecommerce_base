9b425d5addc65f2082d9947aee57bef7a5f779e8
biruk <birukendalew090@gmail.com>
2024-12-13 15:42:17 +0300

    udpate urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from . import views
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 
 app_name = 'accounts'
@@ -7,4 +8,5 @@ app_name = 'accounts'
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.user_login, name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
