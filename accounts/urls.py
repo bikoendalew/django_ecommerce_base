ff3f3815e05fb26599c8a4e6149b3c0253cd2b2a
biruk <birukendalew090@gmail.com>
2024-12-13 14:30:41 +0300

    updating urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,7 @@ from django.urls import path
 app_name = 'accounts'
 
 urlpatterns = [
-    path('/',views.home,name='home'),
+    path('',views.home,name='home'),
     path('register/', views.register, name='register'),
     path('login/', views.user_login, name='login'),
 ]
