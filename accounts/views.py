7920d5c9d0cc536bca6d9bdc5b3a4627cce1e68a
biruk <birukendalew090@gmail.com>
2024-12-13 11:37:04 +0300

    create view for login and register user

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate
 from .form import CustomUserCreationForm
 
 def register(request):
