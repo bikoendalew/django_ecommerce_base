20626855a9de570e079e82c243f7e16176a5674d
biruk <birukendalew090@gmail.com>
2024-12-13 12:03:23 +0300

    adding urls

---
+++ accounts/urls.py
@@ -0,0 +1,11 @@
+from . import views
+from django.urls import path
+
+
+app_name = 'accounts'
+
+urlpatterns = [
+    path('/',views.home,name='home'),
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+]
