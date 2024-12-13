b259f73ee822081e49610e9a3ab3a3dd647a777c
biruk <birukendalew090@gmail.com>
2024-12-13 16:37:07 +0300

    add custom user to adminpage

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'is_active', 'is_staff')
+
+admin.site.register(CustomUser, CustomUserAdmin)
